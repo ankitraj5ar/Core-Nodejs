636d07ae228eed9b325124c78732d8717f791b47
Ankit Raj <ankitraj5ar@gmail.com>
2025-08-14 09:11:32 +0530

    working on streams

--- Streams/writemany.js
+++ Streams/writemany.js
@@ -27,7 +27,25 @@
 //   });
 // })();
 
-// Dont do this
+// // Don't do this
+// import fs from "node:fs/promises";
+// // it takes almost 400 ms to run
+// // CPU uses 30% of the cpu
+// // Memory Usage is about 150 MB
+// (async () => {
+//   console.time("writeMany");
+//   const fileHandle = await fs.open("test.txt", "w");
+//   const stream = fileHandle.createWriteStream();
+
+//   for (let index = 0; index < 1000000; index++) {
+//     const buff = Buffer.from(`index ${index + 1} \n`, "utf-8");
+//     stream.write(buff);
+//   }
+//   fileHandle.close();
+//   console.timeEnd("writeMany");
+// })();
+
+// Stream memory issue fixed
 import fs from "node:fs/promises";
 // it takes almost 400 ms to run
 // CPU uses 30% of the cpu
@@ -37,10 +55,24 @@ import fs from "node:fs/promises";
   const fileHandle = await fs.open("test.txt", "w");
   const stream = fileHandle.createWriteStream();
 
-  for (let index = 0; index < 1000000; index++) {
-    const buff = Buffer.from(`index ${index + 1} \n`, "utf-8");
-    stream.write(buff);
-  }
-  fileHandle.close();
+  console.log(stream.writableHighWaterMark);
+  // console.log(stream.writableLength);
+  const buff = Buffer.alloc(65535, 12);
+  console.log(stream.write(buff));
+  console.log(stream.write(Buffer.alloc(1, "a")));
+
+  stream.on("drain", () => {
+    console.log(stream.write(Buffer.alloc(65535, 12)));
+    console.log(stream.write(Buffer.alloc(1, "a")));
+    console.log("we are now safe to write.");
+  });
+  // console.log(stream.writableLength);
+
+  // for (let index = 0; index < 1000000; index++) {
+  //   const buff = Buffer.from(`index ${index + 1} \n`, "utf-8");
+  //   stream.write(buff);
+  // }
+
+  // fileHandle.close();
   console.timeEnd("writeMany");
 })();
