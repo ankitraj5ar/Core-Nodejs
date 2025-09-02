c553e20dc93fbfaf8375b4215a90776411f2017b
Ankit Raj <ankitraj5ar@gmail.com>
2025-09-02 07:56:35 +0530

    custom writable implemnted

--- Streams/customWritable/custom-writable.js
+++ Streams/customWritable/custom-writable.js
@@ -1,6 +1,5 @@
 import { Writable } from "node:stream";
 import fs from "node:fs";
-import { callbackify } from "node:util";
 class FileWriteStream extends Writable {
   constructor({ highWaterMark, fileName }) {
     super({ highWaterMark });
@@ -13,24 +12,23 @@ class FileWriteStream extends Writable {
 
   // this will run after the constructor and it will put off calling all the other methods until we call the callback function
   _construct(callback) {
-    fs.open(this.fileName, "w", (err, fd) => {
+    fs.open(this.fileName, "w", (err, fileDescriptor) => {
       if (err) {
         // so if we call the callback with an argument , it means that we have an error
         // and we should not be proceed
         return callback(err);
       } else {
-        this.fd = fd;
+        this.fd = fileDescriptor;
+        // no argument means it was successful
+        callback();
       }
     });
-    // no argument means it was successful
-    callback();
   }
 
   _write(chunk, encoding, callback) {
     // do our write operation
     this.chunks.push(chunk);
     this.chunksSize += chunk.length;
-
     if (this.chunksSize > this.writableHighWaterMark) {
       fs.write(this.fd, Buffer.concat(this.chunks), (err) => {
         if (err) {
@@ -46,16 +44,19 @@ class FileWriteStream extends Writable {
       callback();
     }
   }
+
   _final(callback) {
     fs.write(this.fd, Buffer.concat(this.chunks), (err) => {
       if (err) {
         return callback(err);
       }
-      ++this.writeCounts;
       this.chunks = [];
+      this.chunksSize = 0;
+      ++this.writeCounts;
       callback();
     });
   }
+
   _destroy(error, callback) {
     console.log("number of writes: ", this.writeCounts);
     if (this.fd) {
@@ -68,13 +69,52 @@ class FileWriteStream extends Writable {
   }
 }
 
-const stream = new FileWriteStream({
-  highWaterMark: 1800,
-  fileName: "text.txt",
-});
+// const stream = new FileWriteStream({
+//   highWaterMark: 1800,
+//   fileName: "text.txt",
+// });
+// // console.log(stream);
+// stream.write(Buffer.from("This is from stream. "));
+// stream.end(Buffer.from("This is our last write"));
+// stream.on("finish", () => {
+//   console.log("finished");
+// });
+// stream.on("drain", () => {
+//   console.log("drain");
+// });
 
-stream.write(Buffer.from("this is from stream"));
-stream.end(Buffer.from("this is our last write"));
-stream.on("drain", () => {
-  console.log();
-});
+(async () => {
+  console.time("writeMany");
+  const stream = new FileWriteStream({
+    fileName: "text.txt",
+  });
+
+  let i = 0;
+  const noOfWrite = 50000;
+  const writeMany = () => {
+    while (i < noOfWrite) {
+      const buff = Buffer.from(` ${i + 1} \n`, "utf-8");
+      //this is is our last write close the stream
+      if (i === noOfWrite - 1) {
+        stream.end(buff);
+        return;
+      }
+      // if stream.write return false stop the loop
+      if (!stream.write(buff)) {
+        break;
+      }
+      i++;
+    }
+  };
+
+  writeMany();
+
+  // resume our stream if internal buffer is full
+  stream.on("drain", () => {
+    console.log("Draining !!!!");
+    writeMany();
+  });
+  stream.on("finish", () => {
+    console.timeEnd("writeMany");
+  });
+})();
